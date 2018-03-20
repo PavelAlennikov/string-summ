de27a4b11f14de31aade3d67e388658d29b0a179
Pavel Alennikov <palennikov@gmail.com>
2018-03-20 23:42:08 +0700

    rework sum method
    sum method now works with string parts instead of bytes.
    add sum methods tests.

---
+++ test/com/github/pavelalennikov/AllTests.java
@@ -0,0 +1,12 @@
+package com.github.pavelalennikov;
+
+import com.github.pavelalennikov.stringutils.SumMethodsTests;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses(SumMethodsTests.class)
+public class AllTests {
+
+}
